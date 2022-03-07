a62527d2fa6eb02a00440d564c5208f7fe8a95c3
sherif elshafeay <sherifelshaf3y@yahoo.com>
2022-03-07 15:05:12 +0200

    landingPage

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -10,7 +10,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <Link className="navbar-brand" href="#">
-          <img src={logodark} className="logo" alt="" />
+          <img src={logodark} alt="" />
         </Link>
         <button
           className="navbar-toggler"
