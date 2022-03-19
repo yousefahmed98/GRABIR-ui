650b2ef523715bd20ccf97105c3cf2529273735a
sherif elshafeay <sherifelshaf3y@yahoo.com>
2022-03-19 16:50:50 +0200

    userimage edit

--- src/pages/profile/Btnpicture.js
+++ src/pages/profile/Btnpicture.js
@@ -1,19 +1,58 @@
-import React, {useState} from 'react'
-import { Modal, Form, Button } from 'react-bootstrap'
-import Picture from './Picture';
+import React, { useState } from "react";
+import { Modal, Form, Button } from "react-bootstrap";
+import Picture from "./Picture";
+import axios from "axios";
 
 function MyVerticallyCenteredModal(props) {
-    const [data,setData] = useState(null)
-    const changedata = (a) => {
-        if(a.target.name === "pic"){
-                setData (a.target.files[0])
-                console.log(data);
+  const [data, setData] = useState(null);
+  
+  const changedata = (a) => {
+    if (a.target.name === "pic") {
+      setData(a.target.files[0]);
+      console.log(
+        data.name,
+        "///////////////////////////////////////////////////"
+      );
+      //   axios.patch(
+      //     `http://127.0.0.1:8000/base/users/${localStorage.getItem("id")}/`,
+      //     { ProfilePic: data },
+      //     {
+      //       headers: {
+      //         'content-type': 'multipart/form-data',
+      //         Authorization: `Bearer ${localStorage.getItem("access")}`,
+      //       },
+      //     }
+      //   )
+      //   .then((res) => console.log(res))
+      //   .catch((err) => console.log(err))
+      // }
+     
+    }
+  };
 
-        };
-    };
-    console.log(data);
-    return (
-        <>
+const submitPic = (a) => {
+  a.preventDefault();
+  console.log(data,"''''''''''''''''''''''''''''''''''''''''''''''")
+  const formData = new FormData();
+  const imagefile = data
+  formData.append("ProfilePic", imagefile);
+  axios.patch(
+    `http://127.0.0.1:8000/base/users/${localStorage.getItem("id")}/`,
+    formData,
+    {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }
+  )
+  .then((res) => {
+    localStorage.removeItem("ProfilePic")
+    localStorage.setItem("ProfilePic", res.data.ProfilePic)
+    window.alert("Profile Picture updated succefully")
+  })
+}
+  return (
+    <>
       <Modal
         {...props}
         size="lg"
@@ -21,43 +60,47 @@ function MyVerticallyCenteredModal(props) {
         centered
       >
         <Modal.Header closeButton className="modal-box">
-          <Modal.Title id="contained-modal-title-vcenter" >
-            Edit
-          </Modal.Title>
+          <Modal.Title id="contained-modal-title-vcenter">Edit</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-
-            <Form.Group controlId="formFile" className="mb-3">
-                <Form.Label></Form.Label>
-                <Form.Control type="file" name="pic" onChange={(a)=> changedata(a)} />
-                <Button onClick={props.onSave} className="btn-add">Save</Button>
-            </Form.Group>
-            <Picture data={data}/>
+          <Form onSubmit={ (a) => submitPic(a)}>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label></Form.Label>
+            <Form.Control
+              type="file"
+              name="pic"
+              onChange={(a) => changedata(a)}
+            />
+            <Button  onClick={props.onSave} className="btn-add mt-2" type="submit">
+              Save
+            </Button>
+          </Form.Group>
+          </Form>
+          <Picture data={data} />
         </Modal.Body>
         <Modal.Footer>
-          <Button onClick={props.onHide} className="btn-add">Close</Button>
+          <Button onClick={props.onHide} href="/myprofile" className="btn-add">
+            Close
+          </Button>
         </Modal.Footer>
       </Modal>
-      </>
-    );
-  }
-  
- export default function Btnpic() {
-    const [modalShow, setModalShow] = React.useState(false);
-  
-    return (
-      <>
-        <Button onClick={() => setModalShow(true)} className="btn-add" >
-          Add/Edit Picture
-        </Button>
-  
-        <MyVerticallyCenteredModal
-          show={modalShow}
-          onHide={() => setModalShow(false)}
-        />
-      </>
-    );
-  }
-  
+    </>
+  );
+}
 
-  
+export default function Btnpic() {
+  const [modalShow, setModalShow] = React.useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setModalShow(true)} className="btn-add">
+        Edit Picture
+      </Button>
+
+      <MyVerticallyCenteredModal
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </>
+  );
+}
--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,106 +1,131 @@
-import { Reviews } from '@mui/icons-material';
-import React, { useState } from 'react'
-import { Tabs, Tab } from 'react-bootstrap'
-import Navbar from '../../components/navbar/navbar';
-import Btnpic from './Btnpicture';
+import { Reviews } from "@mui/icons-material";
+import React, { useState } from "react";
+import { Tabs, Tab, Stack } from "react-bootstrap";
+import Navbar from "../../components/navbar/navbar";
+import Btnpic from "./Btnpicture";
+import IconButton from "@mui/material/IconButton";
+import CardHeader from "@mui/material/CardHeader";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import Edit from "./Edit";
+import Editadd from "./Edit copy";
 
-import Edit from './Edit'
-import Editadd from './Edit copy';
 // import Picture from './Picture';
 // import Picture from './picture';
-import './profile.css'
-import StarRating from "../../components/StarRating/StarRating"
+import "./profile.css";
+import StarRating from "../../components/StarRating/StarRating";
+import Picture from "./Picture";
 export default function Profile() {
-    const [key, setKey] = useState('home');
-  
-    return (
-    <div class="profile">
-        <Navbar />
-        {/* <Picture  /> */}
-        <Btnpic />
-        <div  className=" tabss">
-      <Tabs 
-        id="controlled-tab-example"
-        activeKey={key}
-        onSelect={(k) => setKey(k)}
-        className="mb-3"
-      >
-        <Tab eventKey="home" title="BASIC INFORMATION" className='tab'>
+  const [key, setKey] = useState("home");
+
+  return (
+    <div className="profile">
+      <Navbar />
+      <div className=" tabss">
+        <Tabs
+          id="controlled-tab-example"
+          activeKey={key}
+          onSelect={(k) => setKey(k)}
+          className="mb-3"
+        >
+          <Tab eventKey="home" title="BASIC INFORMATION" className="tab">
             <>
-            <div>
-                <h4>
-                  Firstname : 
-               </h4>
-                 <p className='text-capitalize'> {localStorage.getItem("firstname")}</p>
-                 <hr/>
-                 <h4> 
-                     Lastname :
-                 </h4>
-                 <p className='text-capitalize'> {localStorage.getItem("lastname")}</p>
-                 <hr/>
-                 <h4>
-                    Username :
-                </h4>
-                 <p className='text-capitalize'> {localStorage.getItem("username")} </p>
-                 <hr/>
-                 <h4>
-                    Region :
-                 </h4>
-                 <p className='text-capitalize'> {localStorage.getItem("region")} </p>
-                 <hr/>
-             </div>
-           <br />
-           <br />
-        <Edit />
+              <Stack direction="horizontal" gap={4}>
+                <div className="">
+                  <CardHeader
+                    avatar={
+                      <img
+                        src={localStorage.getItem("ProfilePic")}
+                        className="mb-1 userImage"
+                        height="140"
+                        alt="deal owner"
+                        loading="lazy"
+                      />
+                    }
+                  />
+                </div>
+                <div className="">
+                  {" "}
+                  <div className="mt-5"><Btnpic /></div>
+                </div>
+              </Stack>
+              <hr />
+
+              <div className="mb-2">{/* <Picture  /> */}</div>
+              <div>
+                <h4>Firstname :</h4>
+                <p className="text-capitalize">
+                  {" "}
+                  {localStorage.getItem("firstname")}
+                </p>
+                <hr />
+                <h4>Lastname :</h4>
+                <p className="text-capitalize">
+                  {" "}
+                  {localStorage.getItem("lastname")}
+                </p>
+                <hr />
+                <h4>Username :</h4>
+                <p className="text-capitalize">
+                  {" "}
+                  {localStorage.getItem("username")}{" "}
+                </p>
+                <hr />
+                <h4>Region :</h4>
+                <p className="text-capitalize">
+                  {" "}
+                  {localStorage.getItem("region")}{" "}
+                </p>
+                <hr />
+              </div>
+              <br />
+              <br />
+              <Edit />
             </>
-        </Tab>
+          </Tab>
 
-
-        {/*  TAB 2 */}
-        <Tab eventKey="profile" title=" Security Information" className='tab'>
-        <>
-            <div>
-                 <h4>
-                    ID :
-                </h4>
-                 <p> {localStorage.getItem("id")}</p>
-                 <hr/>
-                 <h4>
-                  Email :
-               </h4>
-                 <p className='text-capitalize'> {localStorage.getItem("email")} </p> 
-                 <hr/>
-                 <h4> 
-                    Password :
-                 </h4>
-                 <p> ********** </p>
-                 <hr/>
-                 <h4>
-                   Date joined :
-                 </h4>
-                 <p> {localStorage.getItem("dateJoined")} </p>
-                 <hr/>
-                 <h4>
-                   Email verfied :
-                 </h4>
-                <p> {localStorage.getItem("isVerfied") === "true" ? "Yes" : "No" } </p>
-                <hr/>
-             </div>
-           <br />
-           <br />
-        <Editadd />
-            </>
-        </Tab>
-
-        <Tab eventKey="review" title="Reviews" className='tab'>
+          {/*  TAB 2 */}
+          <Tab eventKey="profile" title=" Security Information" className="tab">
             <>
-            <h3>Rates & comments : </h3>
-
-            <StarRating/>
+              <div>
+                <h4>ID :</h4>
+                <p> {localStorage.getItem("id")}</p>
+                <hr />
+                <h4>Email :</h4>
+                <p className="text-capitalize">
+                  {" "}
+                  {localStorage.getItem("email")}{" "}
+                </p>
+                <hr />
+                <h4>Password :</h4>
+                <p> ********** </p>
+                <hr />
+                <h4>Date joined :</h4>
+                <p> {localStorage.getItem("dateJoined")} </p>
+                <hr />
+                <h4>Email verfied :</h4>
+                <p>
+                  {" "}
+                  {localStorage.getItem("isVerfied") === "true"
+                    ? "Yes"
+                    : "No"}{" "}
+                </p>
+                <hr />
+              </div>
+              <br />
+              <br />
+              <Editadd />
             </>
-        </Tab>
-      </Tabs>
+          </Tab>
+
+          <Tab eventKey="review" title="Reviews" className="tab">
+            <>
+              <h3>Rates & comments : </h3>
+
+              <StarRating />
+            </>
+          </Tab>
+        </Tabs>
       </div>
-      </div>
-    );
-  }
+    </div>
+  );
+}
